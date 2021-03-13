1d17cf91840262b4ef6f2ed50ff8b26b46f7da1a
Florian Raabe <florian.raabe@tum.de>
2021-03-13 20:48:15 +0100

    naughty slin

--- src/main/java/com/example/demo/core/MainListener.java
+++ src/main/java/com/example/demo/core/MainListener.java
@@ -43,6 +43,7 @@ public class MainListener extends ListenerAdapter {
         String[] args = message.substring(prefix.length()).trim().split(" ", 2);
 
         if(args[0].equals("naughty") && args[1].equals("slin")) {
+            channel.sendMessage(onlyfans()).queue();
             return;
         }
 
@@ -85,8 +86,9 @@ public class MainListener extends ListenerAdapter {
 
     private MessageEmbed onlyfans() {
         EmbedBuilder builder = new EmbedBuilder();
-        builder.setTitle("Main help page");
-        builder.setDescription(prefix + "mangos\n" + prefix + "help <mango>");
+        builder.setTitle("Subscribe to my Onlyfans!");
+        builder.setDescription("https://onlyfans.com/schnils69");
+        builder.setThumbnail("https://cdn.pocket-lint.com/r/s/1200x/assets/images/153545-apps-news-feature-what-is-onlyfans-and-how-does-it-work-image2-sisy2dmz3f.JPG");
         return builder.build();
     }
 
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,15 @@
 /*
-package com.example.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
 */
