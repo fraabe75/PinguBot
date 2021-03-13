e6e752e143e226e019842512683fb5b795691447
Florian Raabe <florian.raabe@tum.de>
2021-03-13 01:48:11 +0100

    initial 3

--- src/main/java/com/example/demo/database/repositories/DescriptionRepository.java
+++ src/main/java/com/example/demo/database/repositories/DescriptionRepository.java
@@ -2,8 +2,6 @@ package com.example.demo.database.repositories;
 
 import com.example.demo.database.entities.DescriptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface DescriptionRepository extends JpaRepository<DescriptionEntity, Long> {
 }
