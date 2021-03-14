8aff9890da708b4535a04a753baaa68e5c151abc
Florian Raabe <florian.raabe@tum.de>
2021-03-14 21:30:03 +0100

    slin

--- src/main/java/com/example/demo/plugins/impl/Nils.java
+++ src/main/java/com/example/demo/plugins/impl/Nils.java
@@ -3,10 +3,17 @@ package com.example.demo.plugins.impl;
 import com.example.demo.plugins.GuildMessageReceivedPlugin;
 import com.example.demo.plugins.Plugin;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.springframework.stereotype.Service;
 
+import java.awt.*;
+
+
+@Service
 public class Nils extends Plugin implements GuildMessageReceivedPlugin {
 
     public Nils() {
@@ -27,6 +34,7 @@ public class Nils extends Plugin implements GuildMessageReceivedPlugin {
 
         if (command.equals("slin")) {
             channel.sendMessage(onlyfans()).queue();
+            channel.sendMessage("<@137294148397957120>").queue();
         }
 
         return true;
@@ -34,7 +42,7 @@ public class Nils extends Plugin implements GuildMessageReceivedPlugin {
 
     private MessageEmbed onlyfans() {
         EmbedBuilder builder = new EmbedBuilder();
-        builder.setAuthor("137294148397957120");
+        builder.setColor(Color.MAGENTA);
         builder.setTitle("Subscribe to my Onlyfans!");
         builder.setDescription("https://onlyfans.com/schnils69");
         builder.setThumbnail("https://cdn.pocket-lint.com/r/s/1200x/assets/images/153545-apps-news-feature-what-is" +
