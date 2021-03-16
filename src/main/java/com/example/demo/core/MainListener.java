4b015afd6d5648c255cedda522960b5eb2930a79
Lukas Döllerer <lukas.doellerer@tum.de>
2021-03-16 22:21:09 +0100

    improvements

--- src/main/java/com/example/demo/core/MainListener.java
+++ src/main/java/com/example/demo/core/MainListener.java
@@ -89,27 +89,23 @@ public class MainListener extends ListenerAdapter {
             return;
         }
 
-        String command, param;
-        switch (args[0]) {
-            case "bp", "pb", "rp", "pr", "eb", "be" -> {
-                args[0] = args[0].substring(0, 1);
-                args[1] = args[0].substring(1);
-            }
-
+        if (Arrays.stream(new String[]{"pb", "bp", "pr", "rp"}).anyMatch(args[0]::contains)) {
+            args[2] = args[1];
+            args[1] = args[0].substring(1, 2);
+            args[0] = args[0].substring(0, 1);
         }
         switch (args[0]) {
             case "start", "play", "new", "game", "p", "end", "termninate", "e" -> {
-                command = args[1];
-                param = (args[0] + " " + args[2]).trim();
-            }
-            default -> {
-                command = args[0];
-                param = (args[1] + " " + args[2]).trim();
+                String tmp = args[1];
+                args[1] = args[0];
+                args[0] = tmp;
             }
         }
+        String param = String.join(" ", args[1], args[2]).trim();
+
         for (GuildMessageReceivedPlugin guildMessageReceivedPlugin : guildMessageReceivedPlugins) {
-            if (((Plugin) guildMessageReceivedPlugin).commands().contains(command)) {
-                if (!guildMessageReceivedPlugin.guildMessageReceived(event, command, param, prefix)) {
+            if (((Plugin) guildMessageReceivedPlugin).commands().contains(args[0])) {
+                if (!guildMessageReceivedPlugin.guildMessageReceived(event, args[0], param.trim(), prefix)) {
                     channel.sendMessage("Couldn't find command! Try 'dp! help'").queue();
                 }
                 return;
